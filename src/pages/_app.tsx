b87244b1ed98f789001a9d184f07d3f8509bdfc8
driellyps <driellys@live.com>
2021-04-25 18:26:46 +0200

    setting play and pause

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -10,14 +10,31 @@ import styles from '../styles/app.module.scss';
 function MyApp({ Component, pageProps }) {
   const [episodeList, setEpisodeList] = useState([]);
   const [currentEpisodeIndex, setCurrentEpisodeIndex] = useState(0);
+  const [isPlaying, setIsPLaying] = useState(false);
 
   function play(episode) {
     setEpisodeList([episode]);
     setCurrentEpisodeIndex(0);
+    setIsPLaying(true);
+  }
+
+  function togglePlay() {
+    setIsPLaying(!isPlaying);
+  }
+
+  function setPlayingState(state: boolean) {
+    setIsPLaying(state);
   }
   
   return (
-    <PlayerContext.Provider value={{ episodeList, currentEpisodeIndex, play }}>
+    <PlayerContext.Provider value={{ 
+        episodeList, 
+        currentEpisodeIndex, 
+        isPlaying, 
+        play, 
+        togglePlay,
+        setPlayingState 
+      }}>
       <div className={styles.wrapper}>
         <main>
           <Header/>
